7da2ca10c87c1d374634836ad3b133a0d0b630ad
mydevby <mydev.by@gmail.com>
2023-01-30 21:57:57 +0300

    index page

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,18 @@
 window.onload = function() {
-	console.log("Page load");
+	if (document.querySelector('.header__user-about') !== null) {
+		let headerUserAbout  = document.querySelector('.header__user-about');
+		let headerUserInformation = document.querySelector('.header__user-information');
+		headerUserAbout.addEventListener('click', function(e) {
+			headerUserInformation.classList.toggle('header__user-information--open');
+		});
+	};
+
+	if (document.querySelector('.filters__btn-providers') !== null) {
+		let filtersBtnProviders  = document.querySelector('.filters__btn-providers');
+		let headerListProviders = document.querySelector('.filters__list-providers');
+		filtersBtnProviders.addEventListener('click', function(e) {
+			filtersBtnProviders.classList.toggle('filters__btn-providers--active');
+			headerListProviders.classList.toggle('filters__list-providers--open');
+		});
+	};
 };
