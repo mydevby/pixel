73d1de64bfc5cc53fce5f99cc636e35f45a48fdb
mydevby <mydev.by@gmail.com>
2023-02-15 14:09:16 +0300

    Las Vegas

--- src/js/app.js
+++ src/js/app.js
@@ -64,16 +64,6 @@ window.onload = function() {
 		});
 	};
 
-	if (document.querySelector('.documents__nav-link') !== null) {
-		let arrNavLinks  = document.querySelectorAll('.documents__nav-link');
-		arrNavLinks.forEach(navLink => {
-			navLink.addEventListener('click', function(e) {
-				document.querySelector('.documents__nav-link--active').classList.remove('documents__nav-link--active');
-				this.classList.add('documents__nav-link--active');		
-			});
-		});
-	};
-
 	if (document.querySelector('.profile__nav-link') !== null) {
 		let arrNavLinks  = document.querySelectorAll('.profile__nav-link');
 		arrNavLinks.forEach(navLink => {
@@ -83,5 +73,84 @@ window.onload = function() {
 			});
 		});
 	};
+
+
+	//Page Documents
+
+	if (document.querySelector('.documents__nav-link') !== null && document.querySelector('.documents__swiper') !== null) {
+		let arrNavDocumentLinks  = document.querySelectorAll('.documents__nav-link');
+		let hashDocument = window.location.hash;
+		if (document.querySelector('.documents__nav-link[href="documents.html' + hashDocument + '"]') !== null) {
+			document.querySelector('.documents__nav-link--active').classList.remove('documents__nav-link--active');
+			document.querySelector('.documents__nav-link[href="documents.html' + hashDocument + '"]').classList.add('documents__nav-link--active');
+		};
+		const SWIPERDOCUMENTS = new Swiper(".documents__swiper", {
+			slidesPerView: 1,
+			effect: "fade",
+			hashNavigation: {
+				watchState: true,
+			},
+		});
+		arrNavDocumentLinks.forEach(navDocumentLink => {
+			navDocumentLink.addEventListener('click', function(e) {
+				//e.preventDefault();
+				let documentIndex = this.dataset.indexDocument;
+				console.log(documentIndex);
+				SWIPERDOCUMENTS.slideTo(parseInt(documentIndex), 0, true);
+				document.querySelector('.documents__nav-link--active').classList.remove('documents__nav-link--active');
+				this.classList.add('documents__nav-link--active');		
+			});
+		});
+	};
+
+
+	//Page Guides
+
+	if (document.querySelector('.guides__nav-link') !== null && document.querySelector('.guides__swiper') !== null) {
+		let arrNavGuideLinks  = document.querySelectorAll('.guides__nav-link');
+		let hashGuide = window.location.hash;
+		if (document.querySelector('.guides__nav-link[href="guides.html' + hashGuide + '"]') !== null) {
+			document.querySelector('.guides__nav-link--active').classList.remove('guides__nav-link--active');
+			document.querySelector('.guides__nav-link[href="guides.html' + hashGuide + '"]').classList.add('guides__nav-link--active');
+		};
+		const SWIPEGUIDES = new Swiper(".guides__swiper", {
+			slidesPerView: 1,
+			effect: "fade",
+			hashNavigation: {
+				watchState: true,
+			},
+		});
+		arrNavGuideLinks.forEach(navGuideLink => {
+			navGuideLink.addEventListener('click', function(e) {
+				let guideIndex = this.dataset.indexGuide;
+				SWIPEGUIDES.slideTo(parseInt(guideIndex), 0, true);
+				document.querySelector('.guides__nav-link--active').classList.remove('guides__nav-link--active');
+				this.classList.add('guides__nav-link--active');		
+			});
+		});
+	};
+
+
+	// Audio
+	const audioPlayer = document.querySelector(".js-audio-player");
+	const audio = new Audio('../files/audio/vegas-live.mp3');
+
+	audio.addEventListener( "loadeddata", () => {
+		audio.volume = 0.75;
+		audio.loop = true;
+	}, false );
+
+	const lasVegasBtn = audioPlayer.querySelector(".js-toggle-las-vegas-btn");
+	lasVegasBtn.addEventListener( "click", () => {
+		if (audio.paused) {
+			lasVegasBtn.classList.remove("play");
+			lasVegasBtn.classList.add("pause");
+			audio.play();
+		} else {
+			lasVegasBtn.classList.remove("pause");
+			lasVegasBtn.classList.add("play");
+			audio.pause();
+		}
+	}, false );
 
 };
