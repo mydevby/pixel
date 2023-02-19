388574fd940c655c5ba7bbe9d17fea96f8dbd417
mydevby <mydev.by@gmail.com>
2023-02-19 15:39:35 +0300

    bonus timer

--- src/js/app.js
+++ src/js/app.js
@@ -153,4 +153,48 @@ window.onload = function() {
 		}
 	}, false );
 
+
+	// Bonus timer
+	if (document.querySelector('.bonuses__timer') !== null ) {
+		function getTimeRemaining(endtime) {
+			const total = Date.parse(endtime) - Date.parse(new Date());
+			const seconds = Math.floor((total / 1000) % 60);
+			const minutes = Math.floor((total / 1000 / 60) % 60);
+			const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
+			const days = Math.floor(total / (1000 * 60 * 60 * 24));
+			
+			return {
+			  total,
+			  days,
+			  hours,
+			  minutes,
+			  seconds
+			};
+		  }
+		  
+		  function initializeClock(endtime) {
+			const hoursDiv = document.querySelector('.js-bonuses-time-hours');
+			const minutesDiv = document.querySelector('.js-bonuses-time-minutes');
+			const secondsDiv = document.querySelector('.js-bonuses-time-seconds');
+		  
+			function updateClock() {
+			  const t = getTimeRemaining(endtime);
+		  
+			  hoursDiv.innerHTML = ('0' + t.hours).slice(-2);
+			  minutesDiv.innerHTML = ('0' + t.minutes).slice(-2);
+			  secondsDiv.innerHTML = ('0' + t.seconds).slice(-2);
+		  
+			  if (t.total <= 0) {
+				clearInterval(timeinterval);
+			  }
+			}
+		  
+			updateClock();
+			const timeinterval = setInterval(updateClock, 1000);
+		  }
+		  
+		  const deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000);
+		  initializeClock(deadline);
+	}
+
 };
